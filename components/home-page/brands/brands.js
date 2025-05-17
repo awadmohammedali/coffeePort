f619d377ecf34539597f33526834711215d24cd9
awad Ibrahim <>
2025-05-17 14:10:15 +0300

    image and description is updated

--- components/home-page/brands/brands.js
+++ components/home-page/brands/brands.js
@@ -5,7 +5,7 @@ export default function Brands() {
   return (
     <section id="brands" className={classes.brands}>
       <Container>
-        <div  className="text-center">
+        <div className="text-center">
           <div data-aos="fade-up">
             <h1>Our Brands </h1>
           </div>
@@ -15,18 +15,20 @@ export default function Brands() {
                 width={100}
                 height={150}
                 fixed
+                className="the-captin-logo"
                 title="The Captin"
                 image="/the-captin-logo.png"
-                description="Ersha Speciality Coffee Indulge in the rich Flavors and aromas of Ersha, our premium coffee blend. Sourced from the finest coffee beans and carefully crafted to bring out the perfect balance of acidity and body, Ersha is a true delight for coffee connoisseurs."
+                description="The Captain Commercial Coffee A reliable choice for businesses, the Captain is a commercial coffee that delivers a consistent and satisfying cup every time. With a balanced flavour profile and a smooth taste, the Captain is perfect for who’s  looking for a high-quality coffee."
               />
             </Col>
             <Col lg={6} data-aos="fade-left">
-              <Product 
+              <Product
                 title="Speciality Coffee"
                 width={500}
+                className="ersha-logo"
                 height={150}
                 image="/ersah-logo.png"
-                description="The Captain Commercial Coffee A reliable choice for businesses, the Captain is a commercial coffee that delivers a consistent and satisfying cup every time. With a balanced flavour profile and a smooth taste, the Captain is perfect for who’s  looking for a high-quality coffee."
+                description="Ersha Speciality Coffee Indulge in the rich Flavors and aromas of Ersha, our premium coffee blend. Sourced from the finest coffee beans and carefully crafted to bring out the perfect balance of acidity and body, Ersha is a true delight for coffee connoisseurs."
               />
             </Col>
           </Row>
--- components/ui/product.js
+++ components/ui/product.js
@@ -1,10 +1,24 @@
 import Image from "next/image";
 import classes from "./product.module.css";
-export default function Product({ title, image, description, width, height }) {
+export default function Product({
+  title,
+  image,
+  description,
+  className,
+  width,
+  height,
+}) {
   return (
     <div className={classes.product}>
       <div className={classes.image}>
-        <Image loading="lazy"  src={image} fill alt="coffee" />
+        <Image
+          loading="lazy"
+          src={image}
+          width={width}
+          height={height}
+          className={className}
+          alt="coffee"
+        />
       </div>
       <h1>{title}</h1>
       <p>{description}</p>
